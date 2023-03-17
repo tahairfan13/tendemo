c5261dbfe5fc4b7c42c8c00afa032a7531200371
perls <perls@pobox.com>
2023-03-16 21:31:31 -0700

    m

--- src/App.js
+++ src/App.js
@@ -6,7 +6,32 @@ import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
 import CssBaseline from "@mui/joy/CssBaseline";
 import Button from "@mui/joy/Button";
 
+
+
+
 function App() {
+  function ModeSwitcher() {
+    const { mode, setMode } = useColorScheme();
+    const [mounted, setMounted] = React.useState(false);
+  
+    React.useEffect(() => {
+      setMounted(true);
+    }, []);
+  
+    if (!mounted) {
+      return null;
+    }
+    return (
+      <Button
+        variant="outlined"
+        color="neutral"
+        onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+      >
+        {mode === 'dark' ? 'Turn light' : 'Turn dark'}
+      </Button>
+    );
+  }
+
   return (
     <React.StrictMode>
       <StyledEngineProvider injectFirst>
@@ -15,8 +40,9 @@ function App() {
           disableTransitionOnChange
           theme={customTheme}
         >
+        
           <CssBaseline />
-
+          <ModeSwitcher />
           <OffersList />
         </CssVarsProvider>
       </StyledEngineProvider>
